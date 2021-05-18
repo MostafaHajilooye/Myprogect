3c6d710479b6821e5b3467ed3c8bb88fe1b28c36
MostafaHajilooye <49359083+MostafaHajilooye@users.noreply.github.com>
2021-05-18 13:41:55 +0430

    Create pj1.kt

---
+++ pj1.kt
@@ -0,0 +1,9 @@
+class father {
+    fun selectMax (i:Int ,j:Int, k:Int ) Int{
+        var max:Int?=null
+        if (i>j) {
+            max = i
+            else max = j
+            if (max>k) print("max=$max")
+            else  print("max= $k")
+                 }
